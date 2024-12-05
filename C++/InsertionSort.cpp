a8f529cc6037906446897e23ce3619ba1b6ce8f0
Vishwajeet Nehra <nehravishwajeet@gmail.com>
2024-12-05 10:55:35 +0530

    Update InsertionSort.cpp

--- C++/InsertionSort.cpp
+++ C++/InsertionSort.cpp
@@ -2,23 +2,20 @@
 using namespace std;
 
 void insertionsort(int array[], int n) {
-    for (int i = 1; i < n; i++) {   
-// Start from the second element
-        int key = array[i];  
- /* The element to be inserted into the sorted portion*/
-        int j = i - 1;  /* Compare it with the sorted portion, starting from the rightmost element */
+    for (int i = 1; i < n; i++) {
+        int key = array[i];
+        int j = i - 1;
         
-        /* Shift elements of the sorted portion that are greater than 'key' to the right*/
         while (j >= 0 && array[j] > key) {
-            array[j + 1] = array[j];  // Shift element to the right
-            j--;  // Move to the next element
+            array[j + 1] = array[j];
+            j--;
         }
         
-        array[j + 1] = key; 
- // Insert 'key' into its correct position
+        array[j + 1] = key;
     }
 }
 
+ 
 
 int main(){
     int List[6]={8,9,5,9,2,5};
